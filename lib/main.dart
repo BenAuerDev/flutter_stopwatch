6259c549cc0949f53070d04f5875185299485989
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-05 17:51:49 +0100

    add provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_stopwatch/src/home_page.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
