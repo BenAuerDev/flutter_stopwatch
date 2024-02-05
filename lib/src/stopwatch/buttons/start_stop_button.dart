484cfaffad36fbee50ee66ff71c936e5fc21ecdb
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-05 17:55:58 +0100

    add control buttons

---
+++ lib/src/stopwatch/buttons/start_stop_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animated_stopwatch/src/controllers/stopwatch_service.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class StartStopButton extends ConsumerWidget {
+  const StartStopButton({super.key, this.onPressed});
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isRunning = ref.watch(stopwatchProvider).isRunning;
+    return ClipOval(
+      child: Material(
+        color: isRunning ? Colors.red[900] : Colors.green[900], // button color
+        child: InkWell(
+          onTap: onPressed,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              isRunning ? 'Stop' : 'Start',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/stopwatch/stopwatch.dart
+++ lib/src/stopwatch/stopwatch.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animated_stopwatch/src/controllers/stopwatch_service.dart';
+import 'package:flutter_animated_stopwatch/src/stopwatch/buttons/reset_button.dart';
+import 'package:flutter_animated_stopwatch/src/stopwatch/buttons/start_stop_button.dart';
 import 'package:flutter_animated_stopwatch/src/stopwatch/stopwatch_ticker_ui.dart';
 
 import 'package:flutter_animated_stopwatch/src/stopwatch/stopwatch_renderer.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Stopwatch extends HookWidget {
+class Stopwatch extends ConsumerWidget {
   const Stopwatch({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final stopwatch = ref.watch(stopwatchProvider.notifier);
+
     return LayoutBuilder(
       builder: (context, constraints) {
         final radius = constraints.maxWidth / 2;
@@ -15,6 +21,30 @@ class Stopwatch extends HookWidget {
           children: [
             StopwatchRenderer(radius: radius),
             StopwatchTickerUi(radius: radius),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SizedBox(
+                width: 80,
+                height: 80,
+                child: StartStopButton(
+                  onPressed: () {
+                    stopwatch.toggleStartStop();
+                  },
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: SizedBox(
+                width: 80,
+                height: 80,
+                child: ResetButton(
+                  onPressed: () {
+                    stopwatch.reset();
+                  },
+                ),
+              ),
+            ),
           ],
         );
       },
