3c1d1a0033edf27ed20fbb85ee041588b6fa2c35
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-05 19:54:54 +0100

    remove wrong import

--- lib/src/stopwatch/buttons/start_stop_button.dart
+++ lib/src/stopwatch/buttons/start_stop_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_animated_stopwatch/src/controllers/stopwatch_service.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class StartStopButton extends ConsumerWidget {
   const StartStopButton({super.key, this.onPressed});
