bc8047b660b834732293a469fd55969064df3988
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-10 11:08:00 +0100

    refactor stopwatch.dart
    Stopwatch is now a  StatelessWidget only holding Renderer, LapList and Controls

--- lib/src/features/stopwatch/presentation/stopwatch.dart
+++ lib/src/features/stopwatch/presentation/stopwatch.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animated_stopwatch/src/features/laps/application/lap_service.dart';
-import 'package:flutter_animated_stopwatch/src/features/stopwatch/application/stopwatch_service.dart';
-import 'package:flutter_animated_stopwatch/src/utils/handle_scaffold_message.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_animated_stopwatch/src/features/stopwatch/presentation/controls/stopwatch_controls.dart';
 
-import 'package:flutter_animated_stopwatch/src/features/stopwatch/presentation/buttons/reset_button.dart';
-import 'package:flutter_animated_stopwatch/src/features/stopwatch/presentation/buttons/start_stop_button.dart';
 import 'package:flutter_animated_stopwatch/src/features/stopwatch/presentation/stopwatch_renderer.dart';
 import 'package:flutter_animated_stopwatch/src/features/stopwatch/presentation/stopwatch_ticker_ui.dart';
 
-class Stopwatch extends ConsumerWidget {
+class Stopwatch extends StatelessWidget {
   const Stopwatch({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final stopwatch = ref.watch(stopwatchStateProvider.notifier);
-
+  Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 140, left: 32, right: 32, bottom: 32),
+      padding: const EdgeInsets.only(
+        top: 140,
+        left: 32,
+        right: 32,
+        bottom: 32,
+      ),
       child: AspectRatio(
         aspectRatio: 0.85,
         child: LayoutBuilder(
@@ -27,45 +25,7 @@ class Stopwatch extends ConsumerWidget {
               children: [
                 StopwatchRenderer(radius: radius),
                 StopwatchTickerUi(radius: radius),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: SizedBox(
-                    width: 80,
-                    height: 80,
-                    child: StartStopButton(
-                      onPressed: () {
-                        stopwatch.toggleStartStop();
-                      },
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomCenter,
-                  child: SizedBox(
-                    width: 80,
-                    height: 40,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        ref.read(lapServiceProvider.notifier).lap();
-
-                        handleScaffoldMessage(context, 'Lap added');
-                      },
-                      child: const Text('Lap'),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.bottomLeft,
-                  child: SizedBox(
-                    width: 80,
-                    height: 80,
-                    child: ResetButton(
-                      onPressed: () {
-                        stopwatch.reset();
-                      },
-                    ),
-                  ),
-                ),
+                const StopwatchControls(),
               ],
             );
           },
