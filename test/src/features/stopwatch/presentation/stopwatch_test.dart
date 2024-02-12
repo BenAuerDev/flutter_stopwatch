ca802d4b72bac3db7c86a1cdbb83d1e4c50396c1
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-02-12 09:48:19 +0100

    refactor stopwatch_test to use expectFind...

--- test/src/features/stopwatch/presentation/stopwatch_test.dart
+++ test/src/features/stopwatch/presentation/stopwatch_test.dart
@@ -1,18 +1,20 @@
-import 'package:flutter_animated_stopwatch/main.dart';
-import 'package:flutter_animated_stopwatch/src/features/stopwatch/application/stopwatch_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'package:flutter_animated_stopwatch/main.dart';
+import 'package:flutter_animated_stopwatch/src/features/stopwatch/application/stopwatch_service.dart';
+
 import '../../../../test_utils/controls_methods.dart';
+import '../../../../test_utils/expect_find.dart';
 
 void main() {
   testWidgets('Stopwatch controls are displayed correctly', (tester) async {
     await tester.pumpWidget(const ProviderScope(child: MyApp()));
 
     // Expect to find the start, Lap and reset buttons
-    expect(find.text('Reset'), findsOneWidget);
-    expect(find.text('Lap'), findsOneWidget);
-    expect(find.text('Start'), findsOneWidget);
+    expectFindTextReset(tester);
+    expectFindTextLap(tester);
+    expectFindTextStart(tester);
   });
 
   testWidgets('isRunning is initially false', (tester) async {
@@ -30,7 +32,7 @@ void main() {
     expect(provider.state.isRunning, false);
   });
 
-  testWidgets('isRunning is set to true after tapping Start', (tester) async {
+  testWidgets('isRunning is set to true after starting', (tester) async {
     final provider = StopwatchState();
 
     await tester.pumpWidget(
@@ -42,8 +44,7 @@ void main() {
       ),
     );
 
-    // await tapStart(tester);
-    await tester.tap(find.text('Start'));
+    provider.start();
     await tester.pump();
     expect(provider.state.isRunning, true);
     provider.stop();
@@ -63,12 +64,10 @@ void main() {
       ),
     );
 
-    // await tapStart(tester);
-    await tester.tap(find.text('Start'));
-    await tester.pump();
-    // expect(find.text('Start'), findsNothing);
-    expect(find.text('Stop'), findsOneWidget);
-    await tapStop(tester);
+    await tapStartStopButton(tester);
+    await tester.pumpAndSettle();
+    expectFindTextStart(tester);
+    await tapStartStopButton(tester);
   });
 
   testWidgets(
@@ -85,11 +84,10 @@ void main() {
       ),
     );
 
-    await tapStart(tester);
+    await tapStartStopButton(tester);
     await tester.pump();
-    await tapStop(tester);
+    await tapStartStopButton(tester);
     await tester.pump();
-    expect(find.text('Stop'), findsNothing);
-    expect(find.text('Start'), findsOneWidget);
+    expectFindTextStart(tester);
   });
 }
